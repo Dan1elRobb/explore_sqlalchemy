5d52765ab26b43a8fd02d62c412a13d8b84d8e4b
danrob0910 <danrob0910@highgateschool.org.uk>
2023-02-09 09:31:36 +0000

    updated console commandas and models

--- console_commands.py
+++ console_commands.py
@@ -2,19 +2,33 @@ from sqlalchemy import create_engine
 
 from sqlalchemy.orm import Session
 
-from models import Person
+from models import Person, Activity
 
-andrew = Person(first_name="Andrew", last_name="Dales")
+people = [Person(first_name="Andrew", last_name="Dales"),
 
-people = [Person(first_name="Chris", last_name="Brolin"), Person(first_name='Vera', last_name="Malcova")]
+          Person(first_name="Chris", last_name="Brolin"),
+
+          Person(first_name='Vera', last_name="Malcova"),
+
+          ]
+
+chess = Activity(name="Chess")
+
+fives = Activity(name="Fives")
+
+outdoor_ed = Activity(name="Outdoor Ed")
+
+people[0].activities.append(chess)
+
+people[0].activities.append(fives)
+
+people[1].activities.append(outdoor_ed)
+
+people[1].activities.append(fives)
 
 engine = create_engine('sqlite:///activities.db', echo=True)
 
-
 with Session(engine) as sess:
-
-    sess.add(andrew)
-
     sess.add_all(people)
 
     sess.commit()
--- models.py
+++ models.py
@@ -1,12 +1,48 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Table, UniqueConstraint, ForeignKey
 
 from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy.orm import relationship
+
 
 Base = declarative_base()
 
 
-class Person(Base):
+person_activity = Table('person_activity',
 
+                        Base.metadata,
+
+                        Column('id', Integer, primary_key=True),
+
+                        Column('activity_id', ForeignKey('activity.id')),
+
+                        Column('person_id', ForeignKey('person.id')),
+
+                        UniqueConstraint('activity_id', 'person_id')
+
+                        )
+
+
+class Activity(Base):
+    __tablename__ = 'activity'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+
+    name = Column(String, unique=True, nullable=False)
+
+    attendees = relationship("Person",
+
+                             secondary=person_activity,
+
+                             order_by='(Person.last_name, Person.first_name)',
+
+                             back_populates="activities")
+
+    def __repr__(self):
+        return f"<Activity({self.name})>"
+
+
+class Person(Base):
     __tablename__ = 'person'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
@@ -15,10 +51,16 @@ class Person(Base):
 
     last_name = Column(String, nullable=False)
 
-    def __repr__(self):
+    activities = relationship("Activity",
 
+                              secondary=person_activity,
+
+                              order_by='Activity.name',
+
+                              back_populates="attendees")
+
+    def __repr__(self):
         return f"<Person({self.first_name} {self.last_name})>"
 
     def greeting(self):
-
         print(f'{self.first_name} says "hello"!')
